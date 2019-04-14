1d3d793a5d7477cdcad115a84b0d7625c910c0dc
Kobe Aerts <kobe.aerts@gmail.com>
2019-04-14 22:02:51 +0200

    Stuff

---
+++ src/playground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.querySelector('[data-test]');
+
+ReactDOM.render(
+    <span>Een qlskflfsfqsfqsf</span>,
+    container,
+);
